d646799fd663bba3f8b0bb8af46b911cc8fff5c3
fooklook <xiashuo.he@foxmail.com>
2015-11-16 21:03:02 +0800

    ui

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class DatabaseSeeder extends Seeder {
 
@@ -13,7 +14,11 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Model::unguard();
-
+		$user = new User();
+		$user->name = 'admin';
+		$user->email = 'xiashuo.he@foxmail.com';
+		$user->password = crypt('admin');
+		$user->save();
 		// $this->call('UserTableSeeder');
 	}
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">阳春站图书管理系统首页</div>
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="{{ url('') }}">管理图书信息</a></li>
+                            <li class="list-group-item"><a href="{{ url('') }}">管理图书借出入信息</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
